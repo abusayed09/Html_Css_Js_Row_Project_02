319052520ca22a0d6b7daf9bb02aeead1773ef3a
abu sayed <mdabusayedbinamin@gmail.com>
2025-12-30 00:08:52 +0600

    update and complete project

--- insex.js
+++ insex.js
@@ -1,13 +1,17 @@
-
 const menuToggle = document.getElementById('mobile-menu');
 const navList = document.getElementById('nav-list');
+const icon = menuToggle.querySelector('i');
 
 menuToggle.addEventListener('click', () => {
     navList.classList.toggle('active');
-    const icon = menuToggle.querySelector('i');
-    icon.classList.toggle('fa-bars');
-    icon.classList.toggle('fa-times');
-});
+
+
+    if (navList.classList.contains('active')) {
+        icon.className = 'fas fa-times'; 
+    } else {
+        icon.className = 'fas fa-bars';
+    }
+}); 
 
 document.getElementById('movingForm').addEventListener('submit', function(e) {
     e.preventDefault();
